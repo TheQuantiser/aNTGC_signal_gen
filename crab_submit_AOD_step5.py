fdd955bd78d3c0016fa8f61070c4e4e890fda72d
Mohammad <abrar.discloses@gmail.com>
2026-01-28 21:25:50 -0600

    first commit

---
+++ crab_submit_AOD_step5.py
@@ -0,0 +1,74 @@
+from CRABClient.UserUtilities import config
+from CRABAPI.RawCommand import crabCommand
+from CRABClient.ClientExceptions import ClientException
+from httplib import HTTPException
+
+from datetime import date
+
+def submit(config):
+    try:
+        crabCommand('submit', config = config)
+    except HTTPException as hte:
+        print "Failed submitting task: %s" % (hte.headers)
+    except ClientException as cle:
+        print "Failed submitting task: %s" % (cle)
+
+
+workarea                               = '/afs/hep.wisc.edu/user/wadud/private/signal_production/CMSSW_10_6_20/src/GeneratorInterface/LHEInterface/aNTGCcrabsubmit'
+mainOutputDir                          = '/store/user/mwadud/aNTGC/crab/'
+
+# pset                                   = 'aNTGC_ZNuNuG_200_UMN_step5_Run2_2016_AODSIM.py'
+# jobName                                = "aNTGC_ZNuNuGamma_200_2016PostVFP_AODSIM"
+# inputDataset                           = '/aNTGC_ZNuNuGamma_200_2016PostVFP_GEN/mwadud-aNTGC_ZNuNuGamma_200_2016PostVFP_HLT-3ab30fc3f54543258c8ebb8fadbb2996/USER'
+# jobName                                = "aNTGC_ZNuNuGamma_600_2016PostVFP_AODSIM"
+# inputDataset                           = '/aNTGC_ZNuNuGamma_600_2016PostVFP_GEN/mwadud-aNTGC_ZNuNuGamma_600_2016PostVFP_HLT-3ab30fc3f54543258c8ebb8fadbb2996/USER'
+
+
+# pset                                   = 'aNTGC_ZNuNuG_200_UMN_step5_Run2_2016_HIPM_AODSIM.py'
+# jobName                                = "aNTGC_ZNuNuGamma_600_2016PreVFP_AODSIM"
+# inputDataset                           = '/aNTGC_ZNuNuGamma_600_2016PreVFP_GEN/mwadud-aNTGC_ZNuNuGamma_600_2016PreVFP_HLT-3ab30fc3f54543258c8ebb8fadbb2996/USER'
+
+
+pset                                   = 'aNTGC_ZNuNuG_200_UMN_step5_Run2_2017_AODSIM.py'
+jobName                                = "aNTGC_ZNuNuGamma_200_2017_AODSIM"
+inputDataset                           = '/aNTGC_ZNuNuGamma_200_2017_GEN/rusack-aNTGC_ZNuNuGamma_200_2017_HLT-1cba7fad1227c919886864247a60b1e3/USER'
+#jobName                                = "aNTGC_ZNuNuGamma_600_2017_AODSIM"
+#inputDataset                           = '/aNTGC_ZNuNuGamma_600_2017/mwadud-aNTGC_ZNuNuGamma_600_2017_HLT-1cba7fad1227c919886864247a60b1e3/USER'
+
+
+#pset                                   = 'aNTGC_ZNuNuG_200_UMN_step5_Run2_2018_AODSIM.py'
+#jobName                                = "aNTGC_ZNuNuGamma_600_2018_AODSIM"
+#inputDataset                           = '/aNTGC_ZNuNuGamma_600_2018_GEN/mwadud-aNTGC_ZNuNuGamma_600_2018_HLT-b403a189a2d057e62e59ed092120c7f4/USER'
+
+
+config                                 = config()
+
+config.General.requestName             = jobName
+config.General.workArea                = workarea
+config.General.transferLogs            = True
+config.General.instance                = 'prod'
+
+config.JobType.pluginName              = 'Analysis'
+config.JobType.psetName                = pset
+config.JobType.maxMemoryMB             = 4000
+config.JobType.maxJobRuntimeMin        = 300
+config.JobType.numCores                = 4
+config.JobType.sendExternalFolder      = True
+config.JobType.sendPythonFolder        = True
+config.JobType.allowUndistributedCMSSW = True
+
+config.Data.inputDataset               = inputDataset
+config.Data.allowNonValidInputDataset  = True
+config.Data.outputDatasetTag           = jobName
+config.Data.inputDBS                   = 'phys03'
+config.Data.splitting                  = 'FileBased'
+config.Data.unitsPerJob                = 1
+config.Data.totalUnits                 = 1000000000
+config.Data.publication                = True
+config.Data.outLFNDirBase              = mainOutputDir
+config.Data.ignoreLocality             = True
+
+config.Site.storageSite                = 'T2_US_Wisconsin'
+config.Site.whitelist                  = ['T3_US*']
+
+submit(config)
